38df3b9ae0919133cc0a978eef403ad99bd6405b
Gustavo Carreno <guscarreno@gmail.com>
2021-07-28 15:31:39 +0100

    Adding the package at version 0.1.0.1

---
+++ packages/pascalcontainer.pas
@@ -0,0 +1,19 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit PascalContainer;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  CodaMinaAVLTree, CodaMinaBPlusTree, CodaMinaBStarTree, CodaMinaBTree, 
+  CodaMinaHashMap, CodaMinaLockFreeHashMap, CodaMinalockfreeQueue, 
+  CodaMinaPriorityQueue, CodaMinaQuadtree, CodaMinaRBTree, CodaMinaSkipList, 
+  CodaMinaSkipList2, CodaMinaTTree, genericCodaMinaSort, 
+  genericCodaMinaSortableLink, murmur3;
+
+implementation
+
+end.
